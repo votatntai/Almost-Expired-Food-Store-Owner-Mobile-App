fc5a278076e6cc49c8fb32c5d438674c6f4c9c39
Janglee <53804912+votatntai@users.noreply.github.com>
2024-01-20 07:41:34 +0700

    Update Constants.dart

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,6 +1,6 @@
 class AppConstant {
   static const isDarkModeOnPref = 'isDarkModeOnPref';
   static const String TOKEN_KEY = 'access_token';
-  static const String API_URL = 'https://expiredfood.azurewebsites.net/';
+  static const String API_URL = 'https://almostexpiredfood.azurewebsites.net/';
   static const String NOTI_COUNT = '';
 }
